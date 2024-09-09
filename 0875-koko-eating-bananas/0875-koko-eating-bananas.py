eab604594c6e37c4699db5bef9d9ee61d7cf99e4
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-09 08:42:05 -0400

    Time: 296 ms (23.51%), Space: 18.2 MB (14.03%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,25 +1,18 @@
-import math 
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # mid = start + (end - start) //2 
-        max_no_of_bananas = max(piles)
-        left, right = 1, max_no_of_bananas
-        k = max_no_of_bananas
+        max_no_bananas = max(piles)
+        left, right = 1, max_no_bananas
+        k = max_no_bananas
 
         while left <= right:
-            mid = left + (right - left)//2
-            hours = 0 
+            mid = left + (right-left)//2
+            hours = 0
             for i in range(len(piles)):
                 hours += math.ceil(piles[i]/mid)
-            # below condition means that, if hours > h then monkey will
-            #  have to eat more banana's per hour to 
-            # finish them all else it can eat slightly slowly and hence right -= 1
+
             if hours > h:
-                left = mid + 1 
+                left = mid + 1
             else:
                 right = mid - 1
                 k = min(k, mid)
-
         return k
-
-        
