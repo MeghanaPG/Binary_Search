605eeb7fafb11fd05d38736967164741a9a46b14
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-09-09 09:27:09 -0400

    Time: 631 ms (13.64%), Space: 72.9 MB (94.28%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,28 +1,24 @@
-from sortedcontainers import SortedDict 
+from sortedcontainers import SortedDict
 
 class TimeMap:
-
     def __init__(self):
         self.key_time_map = {}
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        #If the key doesn't exist in the dictionary, then we will create a new key 
+    def set(self, key:str, value:str, timestamp: int) -> None:
         if not key in self.key_time_map:
             self.key_time_map[key] = SortedDict()
-        
-        #Will store key as (timestamp, value) pair in 'key' bucket
+
         self.key_time_map[key][timestamp] = value 
 
-    def get(self, key: str, timestamp: int) -> str:
-        # If the key is not present, then we will return empty string 
+    def get(self, key:str, timestamp: int) -> str:
         if not key in self.key_time_map:
             return ""
         
         myIter = self.key_time_map[key].bisect_right(timestamp)
-        # If myIter points to first element it means, no time <= timestamp exits
+
         if myIter == 0:
             return ""
-        
+
         return self.key_time_map[key].peekitem(myIter-1)[1]
 
 
